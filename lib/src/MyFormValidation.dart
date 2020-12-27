87f2e6d96327a743cbd4371df4eb5b7e51624354
unknown <juhyeon1114@gmail.com>
2020-12-27 16:14:18 +0900

    log : TextField, AlertDialog까지 들음

--- lib/src/MyFormValidation.dart
+++ lib/src/MyFormValidation.dart
@@ -7,6 +7,21 @@ class MyFormValidation extends StatefulWidget {
 
 class _MyFormValidationState extends State<MyFormValidation> {
   final _formKey = GlobalKey<FormState>();
+  FocusNode nameFocusNode;
+  final nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    nameFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    nameFocusNode.dispose(); // FocusNode는 사용하지 않을 때는 해제 해줘야됨
+    nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +34,7 @@ class _MyFormValidationState extends State<MyFormValidation> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              TextFormField(
+              TextFormField( // TextFormField와 TextField의 차이점은 validator가 있냐 없냐
                 validator: (value) {
                   return value.isEmpty ? '공백은 허용되지 않습니다.' : null;
                 },
@@ -33,6 +48,26 @@ class _MyFormValidationState extends State<MyFormValidation> {
                 },
                 child: Text('완료')
               ),
+              TextField(
+                controller: nameController,
+                focusNode: nameFocusNode,
+                autofocus: true,
+                decoration: InputDecoration(hintText: '이름을 입력하세요', border: InputBorder.none, labelText: '이름'),
+                onChanged: (text) {print(text);},
+              ),
+              RaisedButton(
+                onPressed: () {
+                  FocusScope.of(context).requestFocus(nameFocusNode);
+                },
+                child: Text('포커스'),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  print(nameController.text);
+                  showDialog(context: context, builder: (context) => AlertDialog(content: Text(nameController.text)));
+                },
+                child: Text('TextField 값 확인'),
+              )
             ],
           )
         )
